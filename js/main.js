4eda41c48e98523d785f96872a9acea67c1cd82d
David11 <romerojesusdavid76@gmail.com>
2024-05-20 15:37:17 -0500

    cambio-3 20/05/2024

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 // Sticky Navigation Menu JS Code
 let nav = document.querySelector("nav");
 let scrollBtn = document.querySelector(".scroll-button a");
-console.log(scrollBtn);
-let val;
+
 window.onscroll = function() {
   if(document.documentElement.scrollTop > 20){
     nav.classList.add("sticky");
@@ -11,16 +10,14 @@ window.onscroll = function() {
     nav.classList.remove("sticky");
     scrollBtn.style.display = "none";
   }
-
 }
-
-//-----------------------------menu de  navegacion---------------------------//
 
 // Side NavIgation Menu JS Code
 let body = document.querySelector("body");
 let navBar = document.querySelector(".navbar");
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn = document.querySelector(".cancel-btn");
+
 menuBtn.onclick = function(){
   navBar.classList.add("active");
   menuBtn.style.opacity = "0";
@@ -28,6 +25,7 @@ menuBtn.onclick = function(){
   body.style.overflow = "hidden";
   scrollBtn.style.pointerEvents = "none";
 }
+
 cancelBtn.onclick = function(){
   navBar.classList.remove("active");
   menuBtn.style.opacity = "1";
@@ -36,17 +34,18 @@ cancelBtn.onclick = function(){
   scrollBtn.style.pointerEvents = "auto";
 }
 
-
-
 // Side Navigation Bar Close While We Click On Navigation Links
-let navLinks = document.querySelectorAll(".menu li a");
-for (var i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("click" , function() {
+let navLinksMenu = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinksMenu.length; i++) {
+  navLinksMenu[i].addEventListener("click" , function() {
     navBar.classList.remove("active");
     menuBtn.style.opacity = "1";
     menuBtn.style.pointerEvents = "auto";
+    body.style.overflow = "auto";
+    scrollBtn.style.pointerEvents = "auto";
   });
 }
+
 
 //---------------------------------funcionamiento del cv --------------------------//
 // Importación de módulos necesarios
