f53fac17b6257479aa662581d7dad38727e7ad96
David11 <romerojesusdavid76@gmail.com>
2024-07-08 18:03:51 -0500

    arreglo del menu responsivo

--- js/main.js
+++ js/main.js
@@ -1,67 +1,65 @@
-// Sticky Navigation Menu JS Code
-let nav = document.querySelector("nav");
-let scrollBtn = document.querySelector(".scroll-button a");
-console.log(scrollBtn);
-let val;
+// Mostrar/Ocultar Botón de Desplazamiento al Inicio
+let scrollButton = document.querySelector(".scroll-button a");
 window.onscroll = function() {
-  if(document.documentElement.scrollTop > 20){
-    nav.classList.add("sticky");
-    scrollBtn.style.display = "block";
-  }else{
-    nav.classList.remove("sticky");
-    scrollBtn.style.display = "none";
+  if (document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
   }
+};
 
-}
-
-//-----------------------------menu de  navegacion---------------------------//
-
-// Side NavIgation Menu JS Code
+// Código adicional para el menú de navegación para que funcione lo seleccionado
 let body = document.querySelector("body");
 let navBar = document.querySelector(".navbar");
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = function(){
+let menuLinks = document.querySelectorAll(".menu a");
+
+menuBtn.onclick = function() {
   navBar.classList.add("active");
   menuBtn.style.opacity = "0";
   menuBtn.style.pointerEvents = "none";
   body.style.overflow = "hidden";
-  scrollBtn.style.pointerEvents = "none";
-}
-cancelBtn.onclick = function(){
+  if (scrollButton) scrollButton.style.pointerEvents = "none";
+};
+
+cancelBtn.onclick = function() {
   navBar.classList.remove("active");
   menuBtn.style.opacity = "1";
   menuBtn.style.pointerEvents = "auto";
   body.style.overflow = "auto";
-  scrollBtn.style.pointerEvents = "auto";
-}
+  if (scrollButton) scrollButton.style.pointerEvents = "auto";
+};
 
-
-
-// Side Navigation Bar Close While We Click On Navigation Links
-let navLinks = document.querySelectorAll(".menu li a");
-for (var i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("click" , function() {
+// Ocultar el menú al hacer clic en cualquier enlace del menú
+menuLinks.forEach(link => {
+  link.onclick = function() {
     navBar.classList.remove("active");
     menuBtn.style.opacity = "1";
     menuBtn.style.pointerEvents = "auto";
-  });
-}
+    body.style.overflow = "auto";
+    if (scrollButton) scrollButton.style.pointerEvents = "auto";
+  };
+});
 
-//---------------------------------funcionamiento del cv --------------------------//
+//------------------------------------------funcionamiento del cv ------------------------------------//
+
+
+
 // Importación de módulos necesarios
-const express = require('express'); // Importa Express, un framework web para Node.js
+import express from 'express'; // Importa Express, un framework web para Node.js
 const app = express(); // Crea una instancia de la aplicación Express
-const fs = require('fs'); // Importa el módulo 'fs' para trabajar con el sistema de archivos
-const path = require('path'); // Importa el módulo 'path' para manejar rutas de archivos
+import { readFileSync, writeFileSync } from 'fs'; // Importa el módulo 'fs' para trabajar con el sistema de archivos
+import { join } from 'path'; // Importa el módulo 'path' para manejar rutas de archivos
+import { post } from 'axios'; // Importa Axios para hacer solicitudes HTTP
 
 // Definición de constantes
-const DOWNLOADS_FILE = path.join(__dirname, 'downloads.json'); // Ruta del archivo para guardar registros de descargas
+const DOWNLOADS_FILE = join(__dirname, 'downloads.json'); // Ruta del archivo para guardar registros de descargas
 
 // Función para obtener las descargas registradas
 function getDownloads() {
   try {
-    const rawData = fs.readFileSync(DOWNLOADS_FILE); // Lee el archivo de registros de descargas
+    const rawData = readFileSync(DOWNLOADS_FILE); // Lee el archivo de registros de descargas
     return JSON.parse(rawData); // Parsea el contenido del archivo JSON
   } catch (err) {
     // Si hay un error al leer el archivo o el archivo no existe, se retorna un objeto vacío
@@ -72,10 +70,10 @@ function getDownloads() {
 // Función para guardar las descargas registradas
 function saveDownloads(downloads) {
   const data = JSON.stringify(downloads, null, 2); // Convierte los registros de descargas a formato JSON
-  fs.writeFileSync(DOWNLOADS_FILE, data); // Escribe los registros en el archivo
+  writeFileSync(DOWNLOADS_FILE, data); // Escribe los registros en el archivo
 }
 
-// Ruta para descargar el CV
+// Ruta para descargar el CV con reCAPTCHA
 app.get('/pdf/CV_Jesus_David_Julio_Romero.pdf', (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress; // Obtiene la dirección IP del cliente
   const downloads = getDownloads(); // Obtiene los registros de descargas
@@ -86,13 +84,33 @@ app.get('/pdf/CV_Jesus_David_Julio_Romero.pdf', (req, res) => {
     return;
   }
 
-  const filePath = path.join(__dirname, 'pdf', 'CV_Jesus_David_Julio_Romero.pdf'); // Ruta del archivo CV
-  res.download(filePath, 'CV_Jesus_David_Julio_Romero.pdf', () => {
-    // Descarga el archivo CV y luego ejecuta esta función de devolución de llamada
-    // Registra la descarga una vez que se ha completado
-    downloads[ip] = true; // Registra la descarga para la dirección IP actual
-    saveDownloads(downloads); // Guarda los registros actualizados en el archivo
-  });
+  // Verificar si el token de reCAPTCHA está presente y válido en la solicitud
+  const recaptchaToken = req.query.token;
+  if (!recaptchaToken) {
+    res.status(400).send('No se proporcionó el token de reCAPTCHA.');
+    return;
+  }
+
+  // Verificar el token de reCAPTCHA usando la API de Google
+  const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=TU_SECRET_KEY&response=${recaptchaToken}`;
+  post(verifyUrl)
+    .then(response => {
+      if (response.data.success) {
+        const filePath = join(__dirname, 'pdf', 'CV_Jesus_David_Julio_Romero.pdf'); // Ruta del archivo CV
+        res.download(filePath, 'CV_Jesus_David_Julio_Romero.pdf', () => {
+          // Descarga el archivo CV y luego ejecuta esta función de devolución de llamada
+          // Registra la descarga una vez que se ha completado
+          downloads[ip] = true; // Registra la descarga para la dirección IP actual
+          saveDownloads(downloads); // Guarda los registros actualizados en el archivo
+        });
+      } else {
+        res.status(403).send('Falló la verificación de reCAPTCHA.');
+      }
+    })
+    .catch(error => {
+      console.error('Error al verificar reCAPTCHA:', error);
+      res.status(500).send('Error interno del servidor al verificar reCAPTCHA.');
+    });
 });
 
 // Ruta de inicio
@@ -102,21 +120,25 @@ app.get('/', (req, res) => {
     <html>
       <head>
         <title>Download CV</title>
+        <script src="https://www.google.com/recaptcha/api.js?render=TU_SITE_KEY"></script>
+        <script>
+          // Función para manejar la descarga del CV con reCAPTCHA
+          function handleDownload() {
+            grecaptcha.ready(function() {
+              // Reemplaza TU_SITE_KEY con tu clave de sitio de reCAPTCHA
+              grecaptcha.execute('TU_SITE_KEY', { action: 'download_cv' }).then(function(token) {
+                // Aquí puedes realizar la descarga del CV si el reCAPTCHA se completó correctamente
+                // Por ejemplo, redirigir al endpoint '/pdf/CV_Jesus_David_Julio_Romero.pdf' donde se encuentra el archivo PDF.
+                window.location.href = '/pdf/CV_Jesus_David_Julio_Romero.pdf?token=' + token;
+              });
+            });
+          }
+        </script>
       </head>
       <body>
         <div class="button">
-          <a id="download-cv-link" href="/pdf/CV_Jesus_David_Julio_Romero.pdf" target="_blank" style="text-decoration: none;">
-            <button id="download-cv-button" onclick="disableButton()">Download CV</button>
-          </a>
+          <button id="download-cv-button" onclick="handleDownload()">Download CV</button>
         </div>
-        <script>
-          // Script de cliente para deshabilitar el botón de descarga después de hacer clic
-          function disableButton() {
-            const button = document.getElementById('download-cv-button'); // Obtiene el botón de descarga
-            button.disabled = true; // Deshabilita el botón después de hacer clic
-            button.innerHTML = 'Downloading...'; // Cambia el texto del botón para indicar la descarga en curso
-          }
-        </script>
       </body>
     </html>
   `); // Envía una respuesta HTML con un botón de descarga y scripts de cliente
@@ -126,6 +148,7 @@ app.get('/', (req, res) => {
 app.listen(5500, () => {
   console.log('Servidor escuchando en el puerto 5500'); // Imprime un mensaje en la consola al iniciar el servidor
 });
+
 
 //-------------------funcionamiento del recaptcha--------///
 
